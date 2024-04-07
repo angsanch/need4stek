b22ae7d9d548a521d976d46e1931d0dead9df312
Angel Daniel Sanchez Garcia <angel-daniel.sanchez-garcia@epitech.eu>
2024-04-07 14:18:24 +0200

    FEAT: move things arround, understand enviroment

--- src/main.c
+++ src/main.c
@@ -6,9 +6,20 @@
 */
 
 #include <stdio.h>
+#include <unistd.h>
+
+void run_command(char const *str)
+{
+    char buff[1024];
+
+    dprintf(1, "%s\n", str);
+    read(0, buff, 1024);
+}
 
 int main(void)
 {
-    puts("START_SIMULATION");
-    puts("STOP_SIMULATION");
+    run_command("start_simulation");
+    run_command("car_forward:0.5");
+    sleep(10);
+    run_command("stop_simulation");
 }
